06a917d3e1d1ac5f7194b6ff3a8c4b372351bf92
Daniel <jdanielosv@hotmail.com>
2021-06-26 12:38:46 -0400

    test if response code is 201 and response code is sent

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,5 @@
-const TodoModel = require("../model/todo.model")
+const TodoModel = require("../model/todo.model");
 exports.createTodo = (req, res, next) => {
-    TodoModel.create(req.body);
+  TodoModel.create(req.body);
+  res.status(201).send();
 };
-
